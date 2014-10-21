aa90a211963775f5eeed6a340a3f59c54cd3e1e8
Amit <amtaylor@gmail.com>
2014-10-21 03:26:13 -0700

    added profiles

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,3 +4,25 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
 
+/* Show Tim Ferriss / Amit Taylor sample profile */
+
+$(function () {
+
+	$(".profile-layout").css("border-top", "2px black dotted");
+	/* $(".profile-layout").hide(); */
+	$(".featured-stacks-side a:first-of-type").click(function() {
+		$(".profile-layout").show();	
+	});
+});
+
+
+
+
+/* replicate first graph 3 times */
+
+$(function () {
+
+	$(".table-side li:first-of-type").clone().appendTo(".table-side ul");
+	$(".table-side li:first-of-type").clone().appendTo(".table-side ul");
+
+});
